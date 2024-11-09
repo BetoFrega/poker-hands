14589a691829c3e7841911f9a037ab1c25d69fb5
betofrega <637874+BetoFrega@users.noreply.github.com>
2024-11-09 01:22:13 -0300

    chore: init storybook

---
+++ src/components/PokerHandsRanker/index.stories.ts
@@ -0,0 +1,17 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import {PokerHandRanker} from './index';
+
+const meta = {
+    title: 'Poker Hand Ranker',
+    component: PokerHandRanker,
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+        layout: 'fullscreen',
+    },
+} satisfies Meta<typeof PokerHandRanker>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/components/PokerHandsRanker/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+export const PokerHandRanker: React.FC = () => {
+
+    return (
+        <div className={styles.container}>
+            <section>
+
+            </section>
+        </div>
+    );
+};
