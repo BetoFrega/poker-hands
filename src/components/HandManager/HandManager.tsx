1d0522197086fb3fc0bfec5d588064b35945e38b
betofrega <637874+BetoFrega@users.noreply.github.com>
2024-11-09 19:47:06 -0300

    feat: select hands

--- src/components/HandManager/HandManager.tsx
+++ src/components/HandManager/HandManager.tsx
@@ -1,12 +1,14 @@
 import React, { ComponentProps, useState } from "react";
 import { cardInArray, removeCardFromArray } from "../../core/actions/cardArray";
 import { Card } from "../../core/types/Card";
+import { cx } from "../../helpers/cx";
 import { CardsSelector } from "../CardsSelector/CardsSelector";
 import { HandDisplay } from "../HandDisplay/HandDisplay";
 import styles from "./HandManager.module.css";
 
 export const HandManager: React.FC = () => {
   const [cards, setCards] = useState<Card[]>([]);
+  const [selectedHand, selectHand] = useState<null | 1 | 2>(null);
   const selectionHandler: ComponentProps<typeof CardsSelector>["onSelect"] = (
     card,
   ) => {
@@ -19,8 +21,48 @@ export const HandManager: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <HandDisplay cards={cards} />
-      <CardsSelector selectedCards={cards} onSelect={selectionHandler} />
+      <div
+        className={cx([
+          styles.handContainer,
+          selectedHand === 1 && styles.selectedHand,
+        ])}
+      >
+        <p>First Hand</p>
+        <HandDisplay cards={cards} />
+        <button
+          className={styles.button}
+          onClick={() => selectHand(1)}
+          disabled={selectedHand === 1}
+        >
+          {selectedHand === 1 ? "Selecting" : "Select"} cards
+        </button>
+      </div>
+      <div
+        className={cx([
+          styles.selectorContainer,
+          !!selectedHand && styles.open,
+        ])}
+      >
+        {selectedHand && <p>Selecting cards for Hand {selectedHand}</p>}
+        <button onClick={() => selectHand(null)}>Close 🅧</button>
+        <CardsSelector selectedCards={cards} onSelect={selectionHandler} />
+      </div>
+      <div
+        className={cx([
+          styles.handContainer,
+          selectedHand === 2 && styles.selectedHand,
+        ])}
+      >
+        <p>Second Hand</p>
+        <HandDisplay cards={cards} />
+        <button
+          className={styles.button}
+          onClick={() => selectHand(2)}
+          disabled={selectedHand === 2}
+        >
+          Select cards
+        </button>
+      </div>
     </div>
   );
 };
