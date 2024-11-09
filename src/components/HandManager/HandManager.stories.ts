21c26a1b4d80474fd5b76cdcee8a5ba6cb344b31
betofrega <637874+BetoFrega@users.noreply.github.com>
2024-11-09 17:26:41 -0300

    chore: add hand manager story

---
+++ src/components/HandManager/HandManager.stories.ts
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { HandManager } from "./HandManager";
+
+const meta: Meta<typeof HandManager> = {
+  component: HandManager,
+};
+export default meta;
+
+export const Default: StoryObj<typeof HandManager> = {};
