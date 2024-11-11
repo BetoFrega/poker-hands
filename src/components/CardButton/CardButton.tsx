b13ed5d2b4521ff56a3b3ff23de631185910d91d
betofrega <637874+BetoFrega@users.noreply.github.com>
2024-11-11 10:28:50 -0300

    feat: button accessibility

--- src/components/CardButton/CardButton.tsx
+++ src/components/CardButton/CardButton.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import { Card, CardSuitEnum } from "../../core/types/Card";
+import { Card, CardSuitEnum, CardValueEnum } from "../../core/types/Card";
 import { cx } from "../../helpers/cx";
 import styles from "./CardButton.module.css";
 
@@ -8,6 +8,27 @@ const suitIconMap: Record<CardSuitEnum, string> = {
   [CardSuitEnum.Clubs]: "♣",
   [CardSuitEnum.Diamonds]: "♦",
   [CardSuitEnum.Spades]: "♠",
+};
+const suitNameMap: Record<CardSuitEnum, string> = {
+  [CardSuitEnum.Hearts]: "Hearts",
+  [CardSuitEnum.Clubs]: "Clubs",
+  [CardSuitEnum.Diamonds]: "Diamonds",
+  [CardSuitEnum.Spades]: "Spades",
+};
+const valueNameMap: Record<CardValueEnum, string> = {
+  [CardValueEnum.Two]: "Two",
+  [CardValueEnum.Three]: "Three",
+  [CardValueEnum.Four]: "Four",
+  [CardValueEnum.Five]: "Five",
+  [CardValueEnum.Six]: "Six",
+  [CardValueEnum.Seven]: "Seven",
+  [CardValueEnum.Eight]: "Eight",
+  [CardValueEnum.Nine]: "Nine",
+  [CardValueEnum.Ten]: "Ten",
+  [CardValueEnum.Jack]: "Jack",
+  [CardValueEnum.Queen]: "Queen",
+  [CardValueEnum.King]: "King",
+  [CardValueEnum.Ace]: "Ace",
 };
 
 export function CardButton({
@@ -22,6 +43,9 @@ export function CardButton({
   const onClickHandler: () => void = useCallback(() => {
     onClick?.(card);
   }, [onClick, card]);
+  const cardName = card
+    ? `${valueNameMap[card.value]} of ${suitNameMap[card.suit]}`
+    : "Empty card";
   return (
     <div
       className={cx([
@@ -31,6 +55,8 @@ export function CardButton({
         onClick && styles.pointer,
       ])}
       onClick={onClickHandler}
+      aria-label={cardName}
+      role="button"
     >
       <p className={styles.CardValue}>{card?.value}</p>
       <p className={styles.CardSuit}>{card && suitIconMap[card.suit]}</p>
