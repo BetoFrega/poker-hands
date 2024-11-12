842ac48bde728d3a2bfc2f704e51b729c06eb847
betofrega <637874+BetoFrega@users.noreply.github.com>
2024-11-12 10:27:04 -0300

    chore(PokerStore): lists all cards in the deck

--- src/core/store/PokerStore.spec.ts
+++ src/core/store/PokerStore.spec.ts
@@ -1,14 +1,32 @@
 import { describe, expect, it } from "@jest/globals";
 import { renderHook } from "@testing-library/react";
 import { useSyncExternalStore } from "react";
+import { Card, CardSuitEnum, CardValueEnum } from "../types/Card";
 
 type StoreListener = () => void;
-type GameState = {};
+type GameState = {
+  deck: {
+    card: Card;
+    /**
+     * If a player has this card in their hand, this field will be set to player number.
+     * Otherwise, it will be undefined.
+     */
+    hand?: 1 | 2;
+  }[];
+};
 
 class PokerStore {
   private static instance: PokerStore;
   private listeners = new Set<StoreListener>();
-  private state = {};
+  private state = {
+    deck: Object.values(CardSuitEnum)
+      .map((suit) => {
+        return Object.values(CardValueEnum).map((value) => {
+          return { card: { suit, value }, hand: undefined };
+        });
+      })
+      .flat(),
+  };
 
   subscribe = (listener: StoreListener) => {
     this.listeners.add(listener);
@@ -38,5 +56,12 @@ describe(PokerStore, () => {
       useSyncExternalStore(pokerStore.subscribe, pokerStore.getSnapshot),
     );
     expect(result).toBeDefined();
+  });
+  it("should list all cards in the deck", () => {
+    const pokerStore = PokerStore.getInstance();
+    const { result } = renderHook(() =>
+      useSyncExternalStore(pokerStore.subscribe, pokerStore.getSnapshot),
+    );
+    expect(result.current.deck).toHaveLength(52);
   });
 });
