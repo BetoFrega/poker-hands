70a173ae5adbf5be1262bc179596d54b958a4a58
betofrega <637874+BetoFrega@users.noreply.github.com>
2024-11-12 11:35:35 -0300

    feat(PokerStore): reset state

--- src/core/store/PokerStore.spec.ts
+++ src/core/store/PokerStore.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "@jest/globals";
+import { beforeEach, describe, expect, it } from "@jest/globals";
 import { act, renderHook } from "@testing-library/react";
 import { produce } from "immer";
 import { useSyncExternalStore } from "react";
@@ -20,22 +20,24 @@ type GameState = {
   }[];
 };
 
+const makeState = () => ({
+  deck: Object.values(CardSuitEnum)
+    .map((suit) => {
+      return Object.values(CardValueEnum).map((value) => {
+        return { card: { suit, value }, hand: undefined };
+      });
+    })
+    .flat(),
+  hands: {
+    player1: [],
+    player2: [],
+  },
+});
+
 class PokerStore {
   private static instance: PokerStore;
   private listeners = new Set<StoreListener>();
-  private store: GameState = {
-    deck: Object.values(CardSuitEnum)
-      .map((suit) => {
-        return Object.values(CardValueEnum).map((value) => {
-          return { card: { suit, value }, hand: undefined };
-        });
-      })
-      .flat(),
-    hands: {
-      player1: [],
-      player2: [],
-    },
-  };
+  private store: GameState = makeState();
 
   subscribe = (listener: StoreListener) => {
     this.listeners.add(listener);
@@ -64,30 +66,56 @@ class PokerStore {
     });
     this.listeners.forEach((listener) => listener());
   };
+
+  reset = () => (this.store = makeState());
 }
 
 describe(PokerStore, () => {
+  const pokerStore = PokerStore.getInstance();
+  beforeEach(() => {
+    pokerStore.reset();
+  });
   it("should be a singleton", () => {
     const pokerStore1 = PokerStore.getInstance();
     const pokerStore2 = PokerStore.getInstance();
     expect(pokerStore1).toBe(pokerStore2);
   });
+  it("should be able to reset its internal state", () => {
+    pokerStore.pickCard(1, {
+      suit: CardSuitEnum.Clubs,
+      value: CardValueEnum.Ace,
+    });
+    pokerStore.pickCard(2, {
+      suit: CardSuitEnum.Diamonds,
+      value: CardValueEnum.Ace,
+    });
+    pokerStore.pickCard(1, {
+      suit: CardSuitEnum.Hearts,
+      value: CardValueEnum.Ace,
+    });
+    pokerStore.pickCard(2, {
+      suit: CardSuitEnum.Spades,
+      value: CardValueEnum.Ace,
+    });
+    expect(pokerStore.getSnapshot().hands.player1).toHaveLength(2);
+    expect(pokerStore.getSnapshot().hands.player2).toHaveLength(2);
+    pokerStore.reset();
+    expect(pokerStore.getSnapshot().hands.player1).toHaveLength(0);
+    expect(pokerStore.getSnapshot().hands.player2).toHaveLength(0);
+  });
   it("should support useSyncExternalStore", () => {
-    const pokerStore = PokerStore.getInstance();
     const { result } = renderHook(() =>
       useSyncExternalStore(pokerStore.subscribe, pokerStore.getSnapshot),
     );
     expect(result).toBeDefined();
   });
   it("should list all cards in the deck", () => {
-    const pokerStore = PokerStore.getInstance();
     const { result } = renderHook(() =>
       useSyncExternalStore(pokerStore.subscribe, pokerStore.getSnapshot),
     );
     expect(result.current.deck).toHaveLength(52);
   });
   it("should allow a user to pick cards", () => {
-    const pokerStore = PokerStore.getInstance();
     const { result } = renderHook(() =>
       useSyncExternalStore(pokerStore.subscribe, pokerStore.getSnapshot),
     );
