1100a35a3e4cc1203e4b76a662afc535f03faee0
betofrega <637874+BetoFrega@users.noreply.github.com>
2024-11-13 16:44:02 -0300

    feat(PokerStore): should return the highest cards in either hand

--- src/store/fixtures.ts
+++ src/store/fixtures.ts
@@ -1,12 +1,18 @@
 import { CardSuitEnum, CardValueEnum } from "../core/types/Card";
 
-export const straightFlushClubs = [
+/**
+ * A straight flush from Ace to Five.
+ */
+export const straightFlushHighFive = [
   { suit: CardSuitEnum.Clubs, value: CardValueEnum.Ace },
   { suit: CardSuitEnum.Clubs, value: CardValueEnum.Two },
   { suit: CardSuitEnum.Clubs, value: CardValueEnum.Three },
   { suit: CardSuitEnum.Clubs, value: CardValueEnum.Four },
   { suit: CardSuitEnum.Clubs, value: CardValueEnum.Five },
 ];
+/**
+ * A full house of Queens and Kings.
+ */
 export const fullHouseQueensKings = [
   { suit: CardSuitEnum.Clubs, value: CardValueEnum.Queen },
   { suit: CardSuitEnum.Diamonds, value: CardValueEnum.Queen },
